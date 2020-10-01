3974a27df621ed148036dc2d51b98619939bc550
Zhlobinchanin <zhlobinchanin@mail.ru>
2020-10-01 23:04:57 +0300

    init: start array task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) min = array[i];
+    if (!array || array.length === 0);
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) max = array[i];
+    if (!array || array.length === 0);
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (!array || array.length === 0);
+    sum = sum + array[i];
+  }
+  let avg = sum/array.length;
+  return avg;
 }
