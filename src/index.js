2d01899b4597e0d7c85039ff11db743b4b4dc572
Zhlobinchanin <zhlobinchanin@mail.ru>
2020-10-01 23:21:39 +0300

    init: start array

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ exports.min = function min (array) {
   let min = 0;
   for (let i = 0; i < array.length; i++) {
     if (min > array[i]) min = array[i];
-    if (!array || array.length === 0);
+    if (!array || array.length === 0)
+    return 0;
   }
   return min;
 }
@@ -12,14 +13,17 @@ exports.max = function max (array) {
   let max = array[0];
   for (let i = 0; i < array.length; i++) {
     if (max < array[i]) max = array[i];
-    if (!array || array.length === 0);
+    if (!array || array.length === 0)
+    return 0;
+  }
   return max;
 }
 
 exports.avg = function avg (array) {
   sum = 0;
   for (let i = 0; i < array.length; i++) {
-    if (!array || array.length === 0);
+    if (!array || array.length === 0)
+    return 0;
     sum = sum + array[i];
   }
   let avg = sum/array.length;
